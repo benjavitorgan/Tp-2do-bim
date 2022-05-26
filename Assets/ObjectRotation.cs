ff61a71a2305524ae380ffc3b912b918407d8382
benjavitorgan <vitorganbenjamin@gmail.com>
2022-05-26 15:52:21 -0300

    Initial commit

---
+++ Assets/ObjectRotation.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectRotation : MonoBehaviour
+{
+    public float RotationSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        RotationSpeed = 3;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0, RotationSpeed, 0);
+        transform.position = new Vector3(0, 0.75f, 0);
+    }
+
+    /*
+    void OnCollisionEnter (Collision col)
+    {
+        if (col.gameObject.name == "Player")
+        {
+            transform.position = new Vector3(0, 0.75f, 0);
+        }  
+    }
+    */
+}
---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float MovementSpeed;
+    public float RotationSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        MovementSpeed = 0.1f;
+        RotationSpeed = 2.5f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.position += new Vector3(0, 0, MovementSpeed);
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.position += new Vector3(0, 0, -MovementSpeed);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.position += new Vector3(-MovementSpeed, 0, 0);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.position += new Vector3(MovementSpeed, 0, 0);
+        }
+
+        if (Input.GetKey(KeyCode.Q))
+        {
+            transform.Rotate(0, -RotationSpeed, 0);
+        }
+
+        if (Input.GetKey(KeyCode.E))
+        {
+            transform.Rotate(0, RotationSpeed, 0);
+        }
+    }
+}
